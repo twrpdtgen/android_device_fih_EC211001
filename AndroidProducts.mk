aa4e9fa28a57e265373224fc1d279fad64d57e93
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-28 19:45:42 +0100

    EC211001: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: fih Model: EC211001 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_EC211001.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_EC211001-user \
+    omni_EC211001-userdebug \
+    omni_EC211001-eng
---
+++ omni_EC211001.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from EC211001 device
+$(call inherit-product, device/fih/EC211001/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := EC211001
+PRODUCT_NAME := omni_EC211001
+PRODUCT_BRAND := Cricket
+PRODUCT_MODEL := EC211001
+PRODUCT_MANUFACTURER := fih
